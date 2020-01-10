b6414626e6ebfe4225ead575dd003567469866ad
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-10 12:23:04 +0300

    added a nav bar

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Dan Brown',
+        'title':'Origins',
+        'content':'Best book',
+        'date_posted':'Jan 03, 2020'
+    },
+    {
+        'author': 'terr Brown',
+        'title':'terry perry',
+        'content':'Best mag',
+        'date_posted':'Jan 23, 2020'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 
