4a9d4c20472f2ddd861a71638fd47ee6dbc92529
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-10 11:55:41 +0300

    added a layout.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>HOMEPAGE</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>ABOUT PAGE</h1>"
+    return render_template('about.html')
 
 
 
