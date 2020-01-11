60d081b8d82cded6798bd4e7573dc1094a4caaf0
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-11 12:40:47 +0300

    changed nav links to activee links

--- flaskblog.py
+++ flaskblog.py
@@ -36,7 +36,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for { form.username.data }!', 'success')
-    # return redirect(url_for('about'))
+    return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
