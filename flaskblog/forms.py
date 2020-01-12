e454347329311d122a1d43e36f1929afa0054264
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-12 13:39:37 +0300

    created a log in instance

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -10,6 +11,19 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken. Please create a new username.')
+
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email already taken. Please create a new email.')
 
 
 class LoginForm(FlaskForm):
